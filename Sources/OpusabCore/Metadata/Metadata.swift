69939137ef8f56ca68ede7ce7ede9e4f8183a3ba
Nikita Starshinov <nstarshinov@yandex.ru>
2019-07-04 00:32:33 +0300

    Add back progress reporting

--- Sources/OpusabCore/Metadata/Metadata.swift
+++ Sources/OpusabCore/Metadata/Metadata.swift
@@ -10,12 +10,22 @@ struct Metadata {
 extension Metadata {
     init(files: [String], verbose: Bool = false) throws {
         precondition(files.count > 0, "At least one input file required")
-        var filesMetadata = try files.map(FileMetadata.init)
+        let progressReporter = ProgressReporter(description: "Parsing metadata", steps: files.count, verbose: verbose)
+        var filesMetadata = try files.map(reporter: progressReporter) { try FileMetadata(file: $0) }
         let title = filesMetadata.first!.album
         let author = filesMetadata.first!.author
         filesMetadata.stripCommonPrefix()
         let chaptersMetadata = [ChapterMetadata](files: filesMetadata)
         let totalDuration = chaptersMetadata.last!.start + filesMetadata.last!.duration
         self.init(title: title, author: author, chapters: chaptersMetadata, totalDuration: totalDuration)
+    }
+}
+
+fileprivate extension Array where Element == String {
+    func map<T>(reporter: ProgressReporter, _ f: (Element) throws -> T ) rethrows -> [T] {
+        return try map {
+            reporter.nextStep(description: $0)
+            return try f($0)
+        }
     }
 }
