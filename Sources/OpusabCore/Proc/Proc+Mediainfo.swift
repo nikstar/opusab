e66b9c89b43ed6e20feb3120cac15c83a7777210
Nikita Starshinov <nstarshinov@yandex.ru>
2019-07-04 03:31:01 +0300

    Use JSON output for mediainfo

--- Sources/OpusabCore/Proc/Proc+Mediainfo.swift
+++ Sources/OpusabCore/Proc/Proc+Mediainfo.swift
@@ -10,31 +10,29 @@ struct MediainfoMetadata {
 
 extension Proc {
     static func mediainfo(_ filename: String) throws -> MediainfoMetadata {
-        let string = try Proc("/usr/local/bin/mediainfo", filename)
+        let data = try Proc("/usr/local/bin/mediainfo", "--Output=JSON", filename)
             .runForStdout()
+            .data(using: .utf8)!
+        let metadata = try! JSONDecoder().decode(Mediainfo.self, from: data)
         
-        //         output example
-        //         ...
-        //             Album                                    : LotR Part II: The Two Towers
-        //             Track name                               : 4.10 The Choices of Master Samwise
-        //             Performer                                : J.R.R. Tolkien
-        //         ...
+        let trackName = metadata.media.track[0].Track ?? (try! File(path: filename).nameExcludingExtension)
+        let album = metadata.media.track[0].Album!
+        let author = metadata.media.track[0].Performer!
         
-        let trackName = extract(tag: "Track name", from: string) ?? (try! File(path: filename).nameExcludingExtension)
-        let album = extract(tag: "Album", from: string)
-        let author = extract(tag: "Performer", from: string)
-        
-        return MediainfoMetadata(trackName: trackName, album: album!, performer: author!)
+        return MediainfoMetadata(trackName: trackName, album: album, performer: author)
     }
+}
 
-    fileprivate static func extract(tag: String, from string: String) -> String? {
-        if let result = string
-            .split(separator: "\n")
-            .first(where: { $0.hasPrefix(tag) })?
-            .split(separator: ":", maxSplits: 1)[1]
-            .dropFirst() {
-            return String(result)
+fileprivate struct Mediainfo: Decodable {
+    var media: Media
+    
+    struct Media: Decodable {
+        var track: [Track]
+        
+        struct Track: Decodable {
+            var Track: String?
+            var Album: String?
+            var Performer: String?
         }
-        return nil
     }
 }
